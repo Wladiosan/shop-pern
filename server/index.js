760204bb02059f762f034ba4a65c869331978d84
Wladiosan <w.ohrimenko@gmail.com>
2022-01-17 20:11:09 +0200

    create models for database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
+
 const sequelize = require('./db')
+const models = require('./models/models')
+
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
 
 const start = async () => {
     try {
@@ -15,5 +21,6 @@ const start = async () => {
         console.log(e)
     }
 }
+
 
 start()
