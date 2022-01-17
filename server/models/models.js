2162869094510c13fa463cc70f79d93734d9d497
Wladiosan <w.ohrimenko@gmail.com>
2022-01-17 23:22:28 +0200

    fixed bug in device table

--- server/models/models.js
+++ server/models/models.js
@@ -21,7 +21,7 @@ const Device = sequelize.define('device', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING, unique: true, allowNull: false},
     price: {type: DataTypes.INTEGER, allowNull: false},
-    rating: {type: DataTypes.STRING, unique: true, allowNull: false},
+    rating: {type: DataTypes.STRING, unique: true, allowNull: true},
     img: {type: DataTypes.STRING, unique: true, allowNull: false}
 })
 
